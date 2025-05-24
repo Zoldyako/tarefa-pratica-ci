4fef276bce31520f2d3e3493838fc365d838fcaa
José Victor <jose.vdr.pessoal@gmail.com>
2025-05-24 18:37:12 -0300

    feat: testa nome, se nota é número

--- src/tests/media.test.js
+++ src/tests/media.test.js
@@ -1,22 +1,40 @@
-import { calcularMedia, resultadoAluno } from '../scripts/calcularNota.js';
+import { resultadoAluno, validaNome, validaNotas } from '../scripts/calcularNota.js';
+
+describe('Testa se o nome é valido', () => {
+    test('Nome valido', () => {
+        expect(validaNome('josé')).toStrictEqual({ ok: true, err: null});
+    });
+
+    test('Nome com números (invalido)', () => {
+        expect(validaNome('José 2')).toStrictEqual({ ok: false, err: '[ERROR]: Nome contém caracteres inválidos.' })
+    });
+});
 
 describe('Testa valores positivos', () => {
     test('Media da nota (4 + 4) / 2 = 4', () => {
-        expect(calcularMedia(4, 4)).toBe(4);
+        expect(resultadoAluno(4, 4)).toStrictEqual({ resultado: 'reprovado', media: 4 });
     });
 
-    test('Media nota (6.5 + 3.3) / 2 = 4.9', () => {
-        expect(calcularMedia(6.5, 3.3)).toBe(4.9);
+    test('Media nota (6 + 7) / 2 = 6.5', () => {
+        expect(resultadoAluno(6, 7)).toStrictEqual({ resultado: 'recuperação', media: 6.5 });
+    });
+
+    test('Media nota (8.9 + 8.9) / 2 = 8.9', () => {
+        expect(resultadoAluno(8.9, 8.9)).toStrictEqual({ resultado: 'aprovado', media: 8.9 });
     });
 }) 
 
-describe('Testa valores negativos e fora de range', () => {
-    test('Media invalida, nota negativa (3, -4', () => {
-        expect(calcularMedia(3, -4)).toBe(false);
+describe('Testa não números, valores fora de range e negativos', () => {
+    test('Testa nota não ser um número, nota maior que 10 (11 + 2)', () => {
+        expect(validaNotas('one', 2)).toStrictEqual({ ok: false, err: '[ERROR]: Nota não é um número.' });
     });
 
     test('Media invalida, nota maior que 10 (11 + 2)', () => {
-        expect(calcularMedia(11, 2)).toBe(false);
+        expect(validaNotas(13, 2)).toStrictEqual({ ok: false, err: '[ERROR]: Nota não pode ser maior que 10.' });
+    });
+
+    test('Media invalida, nota negativa (3, -4', () => {
+        expect(validaNotas(3, -4)).toStrictEqual({ ok: false, err: '[ERROR]: Nota não pode ser negativa.' });
     });
 })
 
